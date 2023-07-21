457e02894bb79b166990ceaa020a1ae434789bbd
Micah Yeager <micah@yeagers.co>
2023-07-21 07:29:10 -0400

    refactor: streamline and reorder tests

---
+++ test-sequencer.ts
@@ -0,0 +1,76 @@
+import { BaseSequencer, type WorkspaceSpec } from "vitest/node"
+
+export class CustomSequencer extends BaseSequencer {
+  public async sort(files: WorkspaceSpec[]) {
+    // not worried about performance since all tests are fast, so for
+    // ease of testing, ensure the following WorkspaceSpecs are run first:
+    // - util/config.spec.ts
+    // - util/fs-helpers.spec.ts
+    // - util/preflight.spec.ts
+    // - util/preflight.add1.spec.ts
+    // - util/preflight.add2.spec.ts
+    // - util/preflight.add3.spec.ts
+    // - initialize.spec.ts
+    // and ensure the following are run last:
+    // - finalize/index.spec.ts
+    // - main.spec.ts
+
+    return files.sort((a, b) => {
+      if (a[1].endsWith("util/config.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("util/config.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("util/fs-helpers.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("util/fs-helpers.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("util/preflight.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("util/preflight.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("util/preflight.add1.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("util/preflight.add1.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("util/preflight.add2.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("util/preflight.add2.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("util/preflight.add3.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("util/preflight.add3.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("initialize.spec.ts")) {
+        return -1
+      }
+      if (b[1].endsWith("initialize.spec.ts")) {
+        return 1
+      }
+      if (a[1].endsWith("finalize/index.spec.ts")) {
+        return 1
+      }
+      if (b[1].endsWith("finalize/index.spec.ts")) {
+        return -1
+      }
+      if (a[1].endsWith("main.spec.ts")) {
+        return 1
+      }
+      if (b[1].endsWith("main.spec.ts")) {
+        return -1
+      }
+      return 0
+    })
+  }
+}
--- vitest.config.mts
+++ vitest.config.mts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vitest/config"
+import { CustomSequencer } from "./test-sequencer"
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   test: {
     globals: true,
     clearMocks: true,
+    bail: 1,
+    sequence: {
+      sequencer: CustomSequencer,
+    },
     coverage: {
+      enabled: true,
       reporter: ["json-summary", "json", "html"],
       all: true,
     },
